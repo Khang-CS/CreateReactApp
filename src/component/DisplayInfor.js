c1e528ab29ead402d0d2c012c92c176dcbcbd55e
khangng72 <khangnguyn07022002@gmail.com>
2024-01-03 09:33:25 +0700

    46-enter project

--- src/component/DisplayInfor.js
+++ src/component/DisplayInfor.js
@@ -1,65 +1,7 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './DisplayInfor.scss';
 import logo from './../logo.svg';
 
-
-
-// class DisplayInfor extends React.Component {
-
-
-//     componentDidMount() {
-//         console.log('>>> Call me component did mount');
-//         setTimeout(() => {
-//             document.title = 'Eric and hehe'
-//         }, 200);
-//     }
-
-//     componentDidUpdate(prevProps, prevState, snapshot) {
-//         console.log('>>> Call me component did update', this.props, prevProps);
-//         if (this.props.listUsers !== prevProps.listUsers) {
-//             if (this.props.listUsers.length === 5) {
-//                 alert('you got 5 users');
-//             }
-//         }
-//     }
-
-
-//     render() {
-//         console.log('>>> call me render');
-//         //props => properties
-
-//         //destructuring arrayy
-//         const { listUsers } = this.props;
-//         return (
-//             <div className='display-infor-container'>
-//                 Display Infor
-//                 <hr></hr>
-
-//                 {true &&
-//                     <div>
-//                         {listUsers.map((user) => {
-
-//                             return (
-//                                 <div key={user.id} className={
-//                                     +user.age > 18 ? "green" : "red"
-//                                 }>
-//                                     <div>My name is {user.name}</div>
-//                                     <div>My age is {user.age}</div>
-
-//                                     <div>
-//                                         <button onClick={() => this.props.handleDeleteUser(user.id)}>Delete</button>
-//                                     </div>
-//                                     <hr />
-//                                 </div>
-//                             )
-
-//                         })}
-//                     </div>
-//                 }
-//             </div>
-//         )
-//     }
-// }
 
 const DisplayInfor = (props) => {
     const { listUsers } = props;
@@ -69,6 +11,17 @@ const DisplayInfor = (props) => {
     // this.state= {
     //     isShowHideListUser: true
     // }
+
+    console.log('call me render');
+
+    useEffect(
+        () => {
+            if (listUsers.length === 0) {
+                alert('You deleted all the users');
+            }
+            console.log('call me use EFFECT');
+        }, [listUsers]
+    );
 
     const handleShowHideListUser = () => {
         setShowHideListUser(!isShowHideListUser);

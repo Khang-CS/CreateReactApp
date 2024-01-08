2ec8b5a3f88f94232729a9b075c7e781e7314b37
khangng72 <khangnguyn07022002@gmail.com>
2024-01-08 15:50:08 +0700

    61

--- src/component/Admin/Content/TableUser.js
+++ src/component/Admin/Content/TableUser.js
@@ -1,13 +1,25 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import { getAllUsers } from "../../../services/apiService";
+
 import axios from 'axios';
 
 const TableUser = (props) => {
 
     const [listUsers, setListUsers] = useState([]);
-    useEffect(() => {
 
+    //componentDidMount
+    useEffect(() => {
+        fetchListUsers();
     }, []);
+
+    const fetchListUsers = async () => {
+        let res = await getAllUsers();
+        if (res.EC === 0) {
+            setListUsers(res.DT);
+        }
+    }
+
 
     return (
         <>Table Users
@@ -19,6 +31,7 @@ const TableUser = (props) => {
                         <th scope="col">Username</th>
                         <th scope="col">Email</th>
                         <th scope="col">Role</th>
+                        <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -30,6 +43,11 @@ const TableUser = (props) => {
                                     <td>{item.username}</td>
                                     <td>{item.email}</td>
                                     <td>{item.role}</td>
+                                    <td>
+                                        <button className='btn btn-secondary'>View</button>
+                                        <button className='btn btn-warning mx-3'>Update</button>
+                                        <button className='btn btn-danger'>Delete</button>
+                                    </td>
                                 </tr>
                             )
                         })
